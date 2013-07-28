0612737a79d22d6a832a94c0c4de0d846d6a242a
Artur Malabarba <bruce.connor.am@gmail.com>
2013-07-28 21:31:57 +0100

    created file

---
+++ greek-unicode-insert.el
@@ -0,0 +1,46 @@
+;;; greek-unicode-insert.el --- Simple keymap to insert Unicode Greek characters.
+
+;; Copyright (C) 2013 Artur Malabarba <bruce.connor.am@gmail.com>
+
+;; Author: Artur Malabarba <bruce.connor.am@gmail.com>
+;; URL: http://github.com/Bruce-Connor/greek-unicode-insert
+;; Version: 1.0
+;; Keywords: 
+;; ShortName: greek-unicode-insert
+;; Separator: /
+
+;;; Commentary:
+;;
+;; 
+
+;;; Instructions:
+;;
+;; INSTALLATION
+;;
+;; If you install from melpa: nothing necessary, should work out of the box.
+;; If you install manually: (require 'greek-unicode-insert)
+
+;;; License:
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+
+;;; Change Log:
+;; 1.0 - 20130728 - Created File.
+;;; Code:
+
+;;;###autoload
+
+
+(provide 'greek-unicode-insert)
+;;; ___package_name___.el ends here.
